afd3694675b2ed24402288bb1d1b18ed71b757ed
Darylaw93 <darylaw93@gmail.com>
2021-06-15 17:29:04 +0800

    day 2 done

--- bird.js
+++ bird.js
@@ -3,27 +3,43 @@ const game = $("<div>").attr("id", "game");
 
 $("body").append(game);
 $("body").append(title);
-$("body").append($("<h2>").attr("id", "score"));
+$("body").append($("<button>").attr("id", "reset").text("Reset Game"));
 
-game.append($("<div>").attr("id", "pipes").addClass("pipes"));
-game.append($("<div>").attr("id", "hole").addClass("hole"));
+game.append(
+  $("<div>").attr("id", "pipes").addClass("pause").addClass("resume")
+);
+game.append($("<div>").attr("id", "hole").addClass("pause").addClass("resume"));
 game.append($("<div>").attr("id", "char"));
-game.append($("<div>").attr("id", "bird").addClass("img"));
+game.append($("<div>").attr("id", "bird").addClass("img").addClass("bird"));
 
-let block = $("#pipe");
+let block = $("#pipes");
 let holes = $("#hole");
 let bird = $("#bird");
+let resetButton = $("#reset");
 let jump = 0;
-let score = -1;
-
+let score = 0;
+let resume = $(".resume");
+$("body").append(
+  $("<h2>")
+    .attr("id", "score")
+    .text("Points: " + score)
+);
 
 // randomise the position of the hole
 // it needs to be between -150px to -450px, the pipe is 500px
 holes.on("animationiteration", () => {
   let random = -(Math.random() * 300 + 150);
   holes.css("top", random);
-  score++
-  $("#score").text("Points: " + score)
+  score++;
+  $("h2").text("Points: " + score);
+});
+
+//reset
+resetButton.click(function () {
+  bird.css("top", 250);
+  resume.addClass("pause");
+  bird.addClass("bird");
+  $("h2").text("Points: " + 0);
 });
 
 ///gravity
@@ -35,13 +51,17 @@ setInterval(function () {
   let left = parseInt($("#pipes").css("left"));
   let hole = parseInt($("#hole").css("top"));
   let birdTop = -(500 - birdHeight);
-// if((birdHeight > 500)||((birdHeight = left)&&(birdHeight = left -50)))
-//collision done with help from google.
-  if ((birdHeight > 500)||((left < 30)&&(left>-50)&&((birdTop<hole)||(birdTop>hole+160)))) {
+  // if((birdHeight > 500)||((birdHeight = left)&&(birdHeight = left -50)))
+  //collision done with help from google.
+  if (
+    birdHeight > 500 ||
+    (left < 20 && left > -50 && (birdTop < hole - 10 || birdTop > hole + 150))
+  ) {
     // alert("Game Over! " + "You scored " + score)
-    score = -1;
-    bird.css("top", 250);
-    game.stop("animationiteration")
+    score = 0;
+    resume.removeClass("pause");
+    bird.removeClass("bird");
+    // alert("Game Over! Your score is " + score)
   }
 }, 10);
 
