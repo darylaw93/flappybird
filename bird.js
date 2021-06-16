5cc6359fe82f6de23d55e2e0ee4eab5a054492b9
Darylaw93 <darylaw93@gmail.com>
2021-06-16 11:58:21 +0800

    hmmm

--- bird.js
+++ bird.js
@@ -1,9 +1,9 @@
 const title = $("<h1>").attr("id", "title").text("Flappy Bird");
 const game = $("<div>").attr("id", "game");
+let button = $("<button>").attr("id", "reset").text("Reset Game");
 
 $("body").append(game);
 $("body").append(title);
-$("body").append($("<button>").attr("id", "reset").text("Reset Game"));
 
 game.append(
   $("<div>").attr("id", "pipes").addClass("pause").addClass("resume")
@@ -12,21 +12,24 @@ game.append($("<div>").attr("id", "hole").addClass("pause").addClass("resume"));
 game.append($("<div>").attr("id", "char"));
 game.append($("<div>").attr("id", "bird").addClass("img").addClass("bird"));
 
-let block = $("#pipes");
-let holes = $("#hole");
-let bird = $("#bird");
-let resetButton = $("#reset");
-let jump = 0;
 let score = 0;
-let resume = $(".resume");
 $("body").append(
   $("<h2>")
     .attr("id", "score")
     .text("Points: " + score)
 );
+$("body").append($("<div>").attr("id", "divButton").append(button));
+
+let block = $("#pipes");
+let holes = $("#hole");
+let bird = $("#bird");
+let resetButton = $("#reset");
+let jump = 0;
+let resume = $(".resume");
 
 // randomise the position of the hole
 // it needs to be between -150px to -450px, the pipe is 500px
+//set numbers to a variable etc 300, 150
 holes.on("animationiteration", () => {
   let random = -(Math.random() * 300 + 150);
   holes.css("top", random);
@@ -39,33 +42,32 @@ resetButton.click(function () {
   bird.css("top", 250);
   resume.addClass("pause");
   bird.addClass("bird");
+  score = 0;
   $("h2").text("Points: " + 0);
 });
 
 ///gravity
-setInterval(function () {
-  let birdHeight = parseInt($("#bird").css("top"));
+let gravity = setInterval(function () {
   if (jump === 0) {
     bird.css("top", "+=3");
   }
+  ////collision
+  let birdHeight = parseInt($("#bird").css("top"));
   let left = parseInt($("#pipes").css("left"));
   let hole = parseInt($("#hole").css("top"));
-  let birdTop = -(500 - birdHeight);
+  let birdVal = -(500 - birdHeight); // negative value due to the "hole" value being negative
   // if((birdHeight > 500)||((birdHeight = left)&&(birdHeight = left -50)))
-  //collision done with help from google.
+  //collision done with help from google & friend.
   if (
     birdHeight > 500 ||
-    (left < 20 && left > -50 && (birdTop < hole - 10 || birdTop > hole + 150))
+    (left < 20 && left > -50 && (birdVal < hole - 10 || birdVal > hole + 150))
   ) {
-    // alert("Game Over! " + "You scored " + score)
-    score = 0;
     resume.removeClass("pause");
     bird.removeClass("bird");
-    // alert("Game Over! Your score is " + score)
   }
 }, 10);
 
-///////Jumping & Jumping
+/////Jumping
 game.on("click", () => {
   let birdHeight = parseInt($("#bird").css("top"));
   jump = 1; //temporarily disables the gravity.
